dda2a8474fbe6180109a7c49cbb32260678a4db8
Supriya Kumari <56879125+12345supu@users.noreply.github.com>
2022-04-17 19:37:46 +0530

    Time: 23 ms (76.90%), Space: 23.2 MB (41.98%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0;
+        for(i=0;i<s.size()/2;i++)
+        {
+            swap(s[i],s[s.size()-i-1]);
+        }
+    }
+};
