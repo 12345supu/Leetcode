9a0e1ca28d8c3e497c5eb247ae63d24d885773ab
Supriya Kumari <56879125+12345supu@users.noreply.github.com>
2022-07-14 10:52:16 +0530

    Added solution - LeetHub

---
+++ Rotate Array - GFG/rotate-array.cpp
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    void arrreverse(int arr[], int s, int e)
+    {
+    while (s < e)
+    {
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+        s++;
+        e--;
+    }
+    }  
+    void rotateArr(int arr[], int d, int n){
+        if (n!=0) 
+        {
+            d=d%n;
+            arrreverse(arr,0,d-1);
+            arrreverse(arr,d,n-1);
+            arrreverse(arr,0,n-1);
+            
+        }
+        // code here
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	//taking testcases
+	cin >> t;
+	
+	while(t--){
+	    int n, d;
+	    
+	    //input n and d
+	    cin >> n >> d;
+	    
+	    int arr[n];
+	    
+	    //inserting elements in the array
+	    for(int i = 0; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    //calling rotateArr() function
+	    ob.rotateArr(arr, d,n);
+	    
+	    //printing the elements of the array
+	    for(int i =0;i<n;i++){
+	        cout << arr[i] << " ";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
