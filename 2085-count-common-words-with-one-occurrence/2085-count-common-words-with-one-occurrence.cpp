503cd1e3b161e763f6ab84b1094ad3e1d694734b
Supriya Kumari <56879125+12345supu@users.noreply.github.com>
2022-07-21 00:22:44 +0530

    Time: 75 ms (24.49%), Space: 20.6 MB (9.40%) - LeetHub

---
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        map<string,int> m1,m2;
+        for(auto x:words1)
+            m1[x]++;
+        for(auto x:words2)
+            m2[x]++;
+
+        int count=0;
+        for(auto x:m2)
+        {
+            if(x.second==1 && m1[x.first]==1){
+                count++;
+        }
+    }
+    return count;
+    }
+};
