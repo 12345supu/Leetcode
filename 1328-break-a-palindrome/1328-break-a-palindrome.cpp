da50335c5fe7f5c23e91660ba29b243d8f71df4e
Supriya Kumari <56879125+12345supu@users.noreply.github.com>
2022-10-11 01:29:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (95.46%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string breakPalindrome(string str) {
+        int n=str.size();
+        if(n==1)
+            return "";
+        for(int i=0; i<(n/2); i++)
+        {
+            if(str[i]!='a')
+            {
+                str[i]='a';
+                return str;
+            }
+        }
+        str[n-1]=str[n-1]+1;
+        return str;
+    }
+};
